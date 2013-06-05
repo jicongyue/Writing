6a761c49195ca9f0dd249f1da4ee02886f81910f
jicongyue <1130285449@qq.com>
2013-06-05 14:06:27 +0800

    完成，在写字区写字，鼠标抬起后4秒会出现在输出区， 按backspace会删除掉输出区的最后一个字

--- Writeting/mian.cpp
+++ Writeting/mian.cpp
@@ -56,8 +56,11 @@ LRESULT CALLBACK WndProc(HWND hwnd,UINT message,
 	static int iWords; //计算保存输出字的个数
 	static HDC hMemDC; //内存DC
 	static HBITMAP hMemBitmap; //内存位图
+	static HDC hdc; //窗口DC
+	static POINT PointStart; //线的起点
+	POINT PointEnd; //线的终点
 	RECT rt;
-	HDC hdc;
+	int iRow,jRow; //表示输出的行列(10行：400/40,15列：600/40)
 	PAINTSTRUCT ps;
 
 	switch(message)
@@ -84,15 +87,82 @@ LRESULT CALLBACK WndProc(HWND hwnd,UINT message,
 		return 0;
 
 	case WM_TIMER:
+		if(bWrite && !bDown) //表明处于写的状态且鼠标抬起
+		{
+			if(iElapse++ > 4) //表明写字结束
+			{
+				if(iWords >= 150) //输出区已满，清除输出区
+				{
+					rt.left = 1;
+					rt.right = 599;
+					rt.top = 31;
+					rt.bottom = 429;
+					FillRect(hMemDC,&rt,WHITE_BRUSH);
+					iWords = 0;
+				}
+				iRow = iWords/15; //行
+				jRow = iWords%15; //列
+				hdc = GetDC(hwnd);
+				StretchBlt(hMemDC,40*jRow+2,40*iRow+32,38,38,
+					hdc,602,32,398,398,SRCCOPY);
+				InvalidateRect(hwnd,NULL,TRUE);
+				ReleaseDC(hwnd,hdc);
+				bWrite = FALSE;
+				bDown = FALSE;
+				++iWords;
+			}
+		}
+		return 0;
+
+	case WM_KEYDOWN:
+		if(VK_BACK == wParam) //按backspace键，删除输出区最后一个字
+		{
+			--iWords;
+			iRow = iWords/15; //行
+			jRow = iWords%15; //列
+			rt.left = 40*jRow+2;
+			rt.right = rt.left+38;
+			rt.top = 40*iRow+32;
+			rt.bottom = rt.top+38;
+			FillRect(hMemDC,&rt,WHITE_BRUSH);
+			InvalidateRect(hwnd,&rt,TRUE);
+		}
 		return 0;
 
 	case WM_MOUSEMOVE:
+		if(bWrite && bDown) //处于写字状态且鼠标左键按下
+		{
+			PointEnd.x = LOWORD(lParam);
+			PointEnd.y = HIWORD(lParam);
+			if(PointEnd.x > 600 && PointEnd.x < 1000
+				&& PointEnd.y > 30 && PointEnd.y < 430)
+			{
+				hdc = GetDC(hwnd);
+				MoveToEx(hdc,PointStart.x,PointStart.y,NULL);
+				LineTo(hdc,PointEnd.x,PointEnd.y);
+				PointStart.x = PointEnd.x;
+				PointStart.y = PointEnd.y;
+				ReleaseDC(hwnd,hdc);
+			}
+		}
 		return 0;
 
 	case WM_LBUTTONDOWN:
+		PointStart.x = LOWORD(lParam);
+		PointStart.y = HIWORD(lParam);
+		if(PointStart.x > 600 && PointStart.x < 1000 
+			&& PointStart.y > 30 && PointStart.y < 430)
+		{
+			bWrite = TRUE; //设置写的状态
+			bDown = TRUE; //表明鼠标按下
+			SetCapture(hwnd);
+		}
 		return 0;
 
 	case WM_LBUTTONUP:
+		bDown = FALSE; //表明鼠标抬起
+		iElapse = 0; //鼠标抬起时开始计时
+		ReleaseCapture();
 		return 0;
 
 	case WM_PAINT:
@@ -117,7 +187,7 @@ LRESULT CALLBACK WndProc(HWND hwnd,UINT message,
 		return 0;
 
 	case WM_SHOWWINDOW:
-		AnimateWindow(hwnd,500,AW_CENTER);
+		AnimateWindow(hwnd,500,AW_CENTER); //显示初始化动画
 		InvalidateRect(hwnd,NULL,TRUE);
 		return 0;
 	}
@@ -134,9 +204,11 @@ void InitWindow(HDC hdcSrc) //初始化窗口
 	rt.top = 0;
 	rt.bottom = 430;
 	FillRect(hdcSrc,&rt,WHITE_BRUSH);
-	Rectangle(hdcSrc,0,0,600,430);
-	Rectangle(hdcSrc,600,0,1000,430);
-	Rectangle(hdcSrc,0,0,1000,30);
+	Rectangle(hdcSrc,0,0,1000,430);
+	MoveToEx(hdcSrc,0,30,NULL);
+	LineTo(hdcSrc,1000,30);
+	MoveToEx(hdcSrc,600,0,NULL);
+	LineTo(hdcSrc,600,430);
 	
 	rt.left = 0;
 	rt.right = 600;
